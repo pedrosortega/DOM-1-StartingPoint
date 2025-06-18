f3a733ea98fa3008423f0dce098e9b501c5597ea
jeramyleon <jeramyleon@gmail.com>
2025-06-18 13:49:00 -0400

    checking off lines 9 and 10

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ console.log("Hello! If you see this, the script is working.");
 - [✓] Select all of the list items with a class of "second".
 - [✓] Select a list item with a class of third, but only the list item inside of the ol tag.
 - [✓] Give the section with an id of container the text "Hello!".
-- [ ] Add the class main to the div with a class of footer.
-- [ ] Remove the class main on the div with a class of footer.
+- [✓] Add the class main to the div with a class of footer.
+- [✓] Remove the class main on the div with a class of footer.
 - [✓] Create a new li element.
 - [✓] Give the li the text "four".
 - [✓] Append the li to the ul element.
